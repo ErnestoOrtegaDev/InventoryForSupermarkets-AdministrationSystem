8b489e114b4aeab9fc2c5ae79795b16c41a304ae
ErnestoOrtega4ABIS <ernestoortega1423@gmail.com>
2026-03-05 19:47:26 -0600

    doc: Change documentation to English for generateToken

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -3,30 +3,30 @@ import { Response } from 'express';
 import { Types } from 'mongoose';
 
 const generateTokens = (res: Response, userId: Types.ObjectId) => {
-  // Crear Access Token (Vida corta: 15 min)
+  // Create Access Token (Short-lived: 15 minutes)
     const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET as string, {
         expiresIn: '15m', 
     });
 
-  // Crear Refresh Token (Vida larga: 7 días)
+  // Create Refresh Token (Long-lived: 1 day)
     const refreshToken = jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET as string, {
         expiresIn: '1d',
     });
 
-  // Cookie del Access Token 
+  // Cookie from Access Token
     res.cookie('jwt', accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite: 'strict',
-        maxAge: 15 * 60 * 1000, 
+        maxAge: 15 * 60 * 1000, // 15 minutes
     });
 
-  // Cookie del Refresh Token 
+  // Cookie from Refresh Token
     res.cookie('jwt-refresh', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite: 'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         path: '/' 
     });
 
